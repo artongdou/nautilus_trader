c80891f8edd2d6b6c8acda472c92759c8e3c1442
cstong <artongdou@gmail.com>
2024-08-08 00:01:36 -0700

    Add SMC Indicator and Strategy

---
+++ playground/smc_strategy.py
@@ -0,0 +1,109 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2024 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
+import pandas as pd
+from nautilus_trader.core.nautilus_pyo3 import LogColor
+from playground.smc_indicator import SmartMoneyConcept
+from nautilus_trader.config import StrategyConfig
+from nautilus_trader.model.book import OrderBook
+from nautilus_trader.model.data import Bar
+from nautilus_trader.model.data import BarType
+from nautilus_trader.model.data import OrderBookDeltas
+from nautilus_trader.model.data import QuoteTick
+from nautilus_trader.model.data import TradeTick
+from nautilus_trader.model.data import BarSpecification
+from nautilus_trader.model.enums import AggregationSource
+from nautilus_trader.model.enums import BarAggregation
+from nautilus_trader.model.enums import BookType
+from nautilus_trader.model.enums import PriceType
+from nautilus_trader.model.identifiers import InstrumentId
+from nautilus_trader.trading.strategy import Strategy
+
+
+class SmartMoneyConceptStrategyConfig(StrategyConfig, frozen=True):
+    """
+    Configuration for ``SmartMoneyConceptStrategy`` instances.
+
+    Parameters
+    ----------
+    instrument_id : InstrumentId
+        The instrument ID for the strategy.
+
+    """
+
+    instrument_id: InstrumentId
+    bar_type: BarType
+
+
+class SmartMoneyConceptStrategy(Strategy):
+    """
+    A strategy that subscribes to bar data and apply SMC strategy
+
+    Parameters
+    ----------
+    config : OrderbookImbalanceConfig
+        The configuration for the instance.
+
+    """
+
+    def __init__(self, config: SmartMoneyConceptStrategyConfig) -> None:
+        super().__init__(config)
+
+        # Configuration
+        self.instrument_id = config.instrument_id
+        self.bar_type = config.bar_type
+
+        # Create indicators
+        self.smc = SmartMoneyConcept(period=5, order_block_count=5)
+
+    def on_start(self) -> None:
+        self.instrument = self.cache.instrument(self.instrument_id)
+        if self.instrument is None:
+            self.log.error(f"Could not find instrument for {self.instrument_id}")
+            self.stop()
+            return
+
+        # Register the indicators for updating
+        self.register_indicator_for_bars(self.bar_type, self.smc)
+
+        # Get historical data
+        self.request_bars(self.bar_type, start=self._clock.utc_now() - pd.Timedelta(days=1))
+
+        # Subscribe to live data
+        # self.subscribe_quote_ticks(self.instrument_id)
+        self.subscribe_bars(self.bar_type)
+
+    def on_order_book_deltas(self, deltas: OrderBookDeltas) -> None:
+        if not self.book:
+            self.log.error("No book being maintained")
+            return
+
+        self.book.apply_deltas(deltas)
+        self.log.info(str(self.book))
+
+    def on_order_book(self, order_book: OrderBook) -> None:
+        self.book = order_book
+        self.log.info(str(self.book))
+
+    def on_trade_tick(self, tick: TradeTick) -> None:
+        self.log.info(str(tick))
+
+    def on_quote_tick(self, tick: QuoteTick) -> None:
+        self.log.info(str(tick))
+
+    def on_bar(self, bar: Bar) -> None:
+        self.log.info(str(bar))
+        self.log.info(f"Order blocks:\t{len(self.smc.get_order_blocks())}", LogColor.GREEN)
+        self.log.info(f"Order blocks:\t{list(self.smc.get_order_blocks())}", LogColor.GREEN)
